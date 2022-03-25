68b376fe6ca4efbd54108935f6bbe31557199155
andrea corronca <andrac1996@gmail.com>
2022-03-25 19:57:18 +0100

    optimized scrollbar listener and minor refactors

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import React,{useState, useEffect} from 'react';
-import { Counter } from './features/counter/Counter';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-import "./App.css"
-
-import requests from "./API_CALL"
-import Header from "./Header"
-import Banner from "./Banner"
-import Row from "./Row"
+import requests from "./API_CALL";
+import Header from "./Header";
+import Banner from "./Banner";
+import Row from "./Row";
 
 function App() {
+  const [isScrolled, setScrolled] = useState(false);
 
-  const [isScrolled, setScrolled] = useState(false)
+  useEffect(() => {
+    const checkScroll = (e) => {
+      if (window.scrollY > 200) {
+        setScrolled(true);
+      }
 
-  window.onscroll = (e) => {
-    if(window.scrollY > 200) setScrolled(true)
+      if (window.scrollY <= 200) setScrolled(false);
+    };
 
-    if(window.scrollY <= 200) setScrolled(false)
-  }
+    window.addEventListener("scroll", checkScroll);
 
+    return () => {
+      window.removeEventListener("scroll", checkScroll);
+    };
+  }, []);
 
   return (
     <div className="App">
       <Header isScrolled={isScrolled}></Header>
       <Banner fetchUrl={requests.netflixMovies}></Banner>
       <div className="rows">
-          <Row fetchUrl={requests.trendingMovies} category="Di moda"></Row>
-          <Row fetchUrl={requests.actionMovies} category="Action"></Row>
-          <Row fetchUrl={requests.animationMovies} category="Animazione"></Row>
-          <Row fetchUrl={requests.comedyMovies} category="Commedia"></Row>
-          <Row fetchUrl={requests.thrillerMovies} category="Thriller"></Row>
-          <Row fetchUrl={requests.horrorMovies} category="Horror"></Row>
+        <Row fetchUrl={requests.trendingMovies} category="Di moda"></Row>
+        <Row fetchUrl={requests.actionMovies} category="Action"></Row>
+        <Row fetchUrl={requests.animationMovies} category="Animazione"></Row>
+        <Row fetchUrl={requests.comedyMovies} category="Commedia"></Row>
+        <Row fetchUrl={requests.thrillerMovies} category="Thriller"></Row>
+        <Row fetchUrl={requests.horrorMovies} category="Horror"></Row>
       </div>
     </div>
   );
--- src/Row.js
+++ src/Row.js
@@ -1,114 +1,114 @@
-import React, {useState, useEffect, useRef} from "react"
+import React, { useState, useEffect, useRef } from "react";
 
-import requests from "./API_CALL"
+import requests from "./API_CALL";
 
-import "./Row.css"
+import "./Row.css";
 
-import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import NavigateBeforeIcon from "@material-ui/icons/NavigateBefore";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
 
+function Row({ fetchUrl, category }) {
+  const [isHovered, setHover] = useState(false);
 
-function Row({fetchUrl, category}) {
+  const [movies, setMovies] = useState([]);
 
-    const [isHovered,setHover] = useState(false)
+  const [isAllScrolled, setAllScrolled] = useState(false);
 
-    const [movies, setMovies] = useState([])
+  const [notScrolled, setNotScrolled] = useState(true);
 
-    const [isAllScrolled, setAllScrolled] = useState(false)
+  const onMouseEnter = () => setHover(true);
+  const onMouseOut = () => setHover(false);
 
-    const [notScrolled, setNotScrolled] = useState(true)
+  const movieRow = useRef(null);
 
-    const onMouseEnter = (() => setHover(true))
-    const onMouseOut = (() => setHover(false))
+  //used to check after pressing the scroll button if the row is totally scrolled
+  const checkAllScrolled = (e) => {
+    let totalLenght = e.scrollWidth;
 
-    const movieRow = useRef(null)
+    let scrolled = e.offsetWidth + e.scrollLeft + e.offsetWidth;
 
-    
-    //used to check after pressing the scroll button if the row is totally scrolled
-    const checkAllScrolled = ((e) => {
+    //check if the row is scrolled to the extreme right, to hide the right scroll button in case
+    //before checking adds the row width, being the animation "smooth" it doesn't istantly update the scrollLeft field
+    if (totalLenght - scrolled < 0) return true;
 
-        let totalLenght = e.scrollWidth
+    return false;
+  };
 
-        let scrolled= e.offsetWidth + e.scrollLeft + e.offsetWidth
+  //scrolls the movie row to the left by its on screen width
+  const scrollMoviesLeft = () => {
+    let e = movieRow.current;
 
-        //check if the row is scrolled to the extreme right, to hide the right scroll button in case
-        //before checking adds the row width, being the animation "smooth" it doesn't istantly update the scrollLeft field
-        if(totalLenght - scrolled < 0) return true
-        
-        return false
+    e.scrollBy({
+      top: 0,
+      left: -e.offsetWidth,
+      behavior: "smooth",
+    });
 
-    })
+    setAllScrolled(false);
+    //check if the row is at the extreme left, to hide the left scroll button in case
+    //before checking subtracts the row width, being the animation "smooth" it doesn't istantly update the scrollLeft field
+    if (e.scrollLeft - e.offsetWidth < 0) setNotScrolled(true);
+  };
 
-    //scrolls the movie row to the left by its on screen width
-    const scrollMoviesLeft = (() => {
+  //scrolls the movie row to the right by its on screen width
+  const scrollMoviesRight = () => {
+    let e = movieRow.current;
 
-        let e = movieRow.current
+    e.scrollBy({
+      top: 0,
+      left: e.offsetWidth,
+      behavior: "smooth",
+    });
 
-        e.scrollBy({
-            top: 0,
-            left: -e.offsetWidth,
-            behavior: "smooth"
-        })
+    setNotScrolled(false);
 
-        setAllScrolled(false)
-        //check if the row is at the extreme left, to hide the left scroll button in case
-        //before checking subtracts the row width, being the animation "smooth" it doesn't istantly update the scrollLeft field
-        if (e.scrollLeft - e.offsetWidth < 0) setNotScrolled(true)
+    if (checkAllScrolled(movieRow.current)) setAllScrolled(true);
+  };
 
-    })
+  useEffect(() => {
+    fetch(fetchUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.results.slice(0, 20));
+      });
+  }, []);
 
-    //scrolls the movie row to the right by its on screen width
-    const scrollMoviesRight = (() => {
-
-        let e = movieRow.current
-
-        e.scrollBy({
-            top: 0,
-            left: e.offsetWidth,
-            behavior: "smooth"
-        })
-
-        setNotScrolled(false)
-
-        if (checkAllScrolled(e)) setAllScrolled(true)
-
-    })
-
-    useEffect(() => {
-        fetch(fetchUrl)
-            .then((response) => response.json())
-            .then((data) => {
-                setMovies(data.results.slice(0,20))
-            })
-    },[])
-
-    return (
-        <div className="row">
-            <p className="rowTitle">{category}</p>
-            <div className="externalContainer" onMouseEnter={onMouseEnter} onMouseLeave={onMouseOut}>
-
-                <div ref={movieRow} className="rowContainer">
-                    {movies.map((movie) => {
-                        return <img key={movie.id} id={movie.id} className="moviePoster" src={requests.imageBase + movie.poster_path} alt=""></img>
-                    })}
-                </div>
-            
-
-
-                {isHovered && !notScrolled &&
-                    <div className="leftArrow arrowBar" onClick={scrollMoviesLeft}>
-                        <NavigateBeforeIcon className="arrowIcon"></NavigateBeforeIcon>
-                    </div>
-                }
-
-                {isHovered && !isAllScrolled &&
-                    <div className="rightArrow arrowBar" onClick={scrollMoviesRight}>
-                        <NavigateNextIcon className="arrowIcon"></NavigateNextIcon>
-                    </div>
-                }        
-            </div>      
+  return (
+    <div className="row">
+      <p className="rowTitle">{category}</p>
+      <div
+        className="externalContainer"
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseOut}
+      >
+        <div ref={movieRow} className="rowContainer">
+          {movies.map((movie) => {
+            return (
+              <img
+                key={movie.id}
+                id={movie.id}
+                className="moviePoster"
+                src={requests.imageBase + movie.poster_path}
+                alt=""
+              ></img>
+            );
+          })}
         </div>
-    )
+
+        {isHovered && !notScrolled && (
+          <div className="leftArrow arrowBar" onClick={scrollMoviesLeft}>
+            <NavigateBeforeIcon className="arrowIcon"></NavigateBeforeIcon>
+          </div>
+        )}
+
+        {isHovered && !isAllScrolled && (
+          <div className="rightArrow arrowBar" onClick={scrollMoviesRight}>
+            <NavigateNextIcon className="arrowIcon"></NavigateNextIcon>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
